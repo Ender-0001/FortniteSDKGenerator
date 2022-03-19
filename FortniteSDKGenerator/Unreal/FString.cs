f0cc515cdf6a28acd23f3e954ce64b019e9bac9b
Ender0001 <70291335+Ender-0001@users.noreply.github.com>
2022-03-18 22:34:12 -0400

    TArray And FString

---
+++ FortniteSDKGenerator/Unreal/FString.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FortniteSDKGenerator
+{
+    class FString : MemoryObject
+    {
+        public FString(ulong InAddress) : base(InAddress)
+        {
+        }
+        public Int32 Length => Read<Int32>(8) * 2;
+        public UInt64 ArrayData => Read<UInt64>(0);
+
+
+        public string Get()
+        {
+            var Bytes = Memory.ReadMemory(ArrayData, Length);
+            return Encoding.Unicode.GetString(Bytes);
+        }
+
+
+    }
+}
---
+++ FortniteSDKGenerator/Unreal/TArray.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FortniteSDKGenerator
+{
+
+    class TArray<T> : MemoryObject
+    {
+        public TArray(ulong InAddress, bool InPtr = true, int InSize = 8) : base(InAddress)
+        {
+            Ptr = InPtr;
+            Size = InSize;
+        }
+
+        public readonly bool Ptr;
+        public readonly int Size;
+
+        public Int32 Count => Read<Int32>(8);
+        public Int32 Max => Read<Int32>(12);
+        public UInt64 ArrayData => Read<UInt64>(0);
+
+        public T GetByIndex(int index)
+        {
+            if (Ptr)
+            {
+                return (T)Activator.CreateInstance(typeof(T), Memory.Read<UInt64>(ArrayData, (8 * index)));
+            }
+            else
+            {
+                return (T)Activator.CreateInstance(typeof(T), Memory.Read<UInt64>(ArrayData, (Size * index)));
+            }
+        }
+
+        public int Num() => Count;
+    }
+}
